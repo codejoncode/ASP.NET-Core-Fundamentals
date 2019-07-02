cb833a55734ed186dba0df7b4bea34ba016b83d7
Jonathan <jonathanjholloway@gmail.com>
2019-07-02 17:38:04 -0500

    adding in some data got a good error message that created thought and problem solving class inaccessible due to protection level. this was because the Restaurant class was not marked as public. Classes are private by default so without marking it as public I could not access it. creating some dummy data, and getting some restaurants on the board for display

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        //see the  OdeToFood.Data.csproj file on how to reference the Restaurant from OdeToFood.Core
+        //unable to reference Restaurant unless the Restaurant class is marked public 
+        /*Experienced  error  Restaurant inaccessible due to protection level error 
+          classes are private by default. Without explicitly calling public it is inaccessible.  
+        */
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant {Id = 1, Name = "Scott's Pizza", Location= "Maryland", Cuisine=CuisineType.Italian},
+                new Restaurant {Id = 1, Name = "Cinnamon Club", Location= "London", Cuisine=CuisineType.Italian},
+                new Restaurant {Id = 1, Name = "La Costa", Location= "Califorina", Cuisine=CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            
+        }
+    }
+}
--- OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -4,15 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace OdeToFood.Pages.Restaurants
 {
     public class ListModel : PageModel
     {
+        private readonly IConfiguration config;
+
         public string Message { get; set; }
+
+        //Constructors should be named the same as the class 
+        public ListModel(IConfiguration config)
+        {
+            //make a private readonly property
+            this.config = config; 
+
+        }
         public void OnGet()
         {
-            Message = "Hello World!";
+            //gets the value from appsettings.json
+            Message = config["Message"];
         }
     }
 }
